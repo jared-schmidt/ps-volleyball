008d6a8fe7ba3e6148fc8c99be44993ead6f8b8f
David Bodenschatz <dbodenschatz@problemsolutions.net>
2016-05-18 09:24:30 -0400

    Moved weather to a modal

--- client/client.js
+++ client/client.js
@@ -230,6 +230,23 @@ Template.hello.helpers({
 });
 
 Template.hello.events({
+    'click #WeatherBtn' : function(e){
+        $('.modal-trigger').leanModal({
+            dismissible: true,
+            ready: function() {
+                if($(".lean-overlay").length > 1) {
+                    $(".lean-overlay:not(:first)").each(function() {
+                        $(this).remove();
+                    });
+                }
+            },
+             complete: function() {
+                $(".lean-overlay").each(function() {
+                    $(this).remove();
+                });
+            }
+        });
+    },
     'click #getPastData': function(e){
         alert('LOOK IN JAVASCRIPT CONSOLE');
         console.log(Meteor.users.find({}, {fields: {'profile.past': 1, 'profile.name': 1}}).fetch());
